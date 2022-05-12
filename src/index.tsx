554fac6a753d8621f1102079efd237a26ee3fcdb
Polaris <93653665+pohjantahti@users.noreply.github.com>
2022-05-12 16:43:43 +0300

    Update root render to React 18 version

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import StartMenu from "./components/view/StartMenu";
 import "./index.css";
 
-ReactDOM.render(
+const container = document.getElementById("root");
+const root = createRoot(container!);
+root.render(
     <React.StrictMode>
         <StartMenu />
-    </React.StrictMode>,
-    document.getElementById("root")
+    </React.StrictMode>
 );
-
-// TODO: Get rid of "any" types
