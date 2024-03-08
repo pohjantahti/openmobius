44814b7156c18d5ca64441327d92862af9e42279
Polaris <93653665+pohjantahti@users.noreply.github.com>
2024-03-08 23:07:19 +0200

    Add options to filter assets in AssetViewer

--- src/pages/asset-viewer/AssetViewer.tsx
+++ src/pages/asset-viewer/AssetViewer.tsx
@@ -1,9 +1,24 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import { RouteOptions } from "../main-menu/Router";
-import { Box, Button, CircularProgress, Paper, Stack, Typography } from "@mui/material";
+import {
+    Box,
+    Button,
+    Chip,
+    CircularProgress,
+    FormControl,
+    IconButton,
+    InputAdornment,
+    InputLabel,
+    OutlinedInput,
+    Paper,
+    Stack,
+    Typography,
+} from "@mui/material";
 import AssetList from "./components/AssetScrollList";
 import { getAsset } from "@extractor/assetExtraction";
 import AssetDisplay from "./components/AssetDisplay";
+import { ClassID } from "@extractor/consts";
+import HighlightOffIcon from "@mui/icons-material/HighlightOff";
 
 interface Props {
     setRoute: React.Dispatch<React.SetStateAction<RouteOptions>>;
@@ -19,8 +34,57 @@ type AssetListJSON = Array<{
     }>;
 }>;
 
+interface AssetListItem {
+    container: string;
+    type: "container" | "asset";
+    name: string;
+    asset: {
+        classId: number;
+        pathId: string;
+    };
+}
+
 function AssetViewer(props: Props) {
     const [assetList, setAssetList] = useState<AssetListJSON>([]);
+    const [filterChips, setFilterChips] = useState<Record<number, boolean>>({
+        28: true,
+        43: true,
+    });
+    const [filterText, setFilterText] = useState("");
+
+    const visibleAssetList = useMemo(() => {
+        const list: Array<AssetListItem> = [];
+        for (const container of assetList) {
+            const tempList: Array<AssetListItem> = [];
+            for (const asset of container.assets) {
+                if (
+                    filterChips[asset.classId] &&
+                    asset.name?.toLowerCase().includes(filterText.toLowerCase())
+                ) {
+                    tempList.push({
+                        container: container.container,
+                        name: asset.name ? asset.name : ClassID[asset.classId],
+                        type: "asset",
+                        asset: {
+                            classId: asset.classId,
+                            pathId: asset.pathId,
+                        },
+                    });
+                }
+            }
+            if (tempList.length > 0) {
+                list.push({
+                    container: container.container,
+                    name: container.name,
+                    type: "container",
+                    asset: { classId: -1, pathId: "" },
+                });
+                list.push(...tempList);
+            }
+        }
+        return list;
+    }, [assetList, filterChips, filterText]);
+
     const inProgress = useRef(false);
     const [displayedAsset, setDisplayedAsset] = useState<{
         url: string;
@@ -47,6 +111,15 @@ function AssetViewer(props: Props) {
             url: url,
             classId: classId,
         });
+    };
+
+    const handleChipFiltering = (classId: 28 | 43) => {
+        filterChips[classId] = !filterChips[classId];
+        setFilterChips({ ...filterChips });
+    };
+
+    const handleTextFiltering = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setFilterText(event.target.value);
     };
 
     return (
@@ -89,11 +162,45 @@ function AssetViewer(props: Props) {
                                 width: 1,
                                 display: "flex",
                                 flexDirection: "column",
-                                justifyContent: "flex-end",
+                                justifyContent: "space-between",
                             }}
                         >
+                            <FormControl variant="outlined" size="small">
+                                <InputLabel>Asset name</InputLabel>
+                                <OutlinedInput
+                                    label="Asset name"
+                                    value={filterText}
+                                    onChange={handleTextFiltering}
+                                    endAdornment={
+                                        filterText.length > 0 && (
+                                            <InputAdornment position="end">
+                                                <IconButton
+                                                    edge="end"
+                                                    onClick={() => setFilterText("")}
+                                                >
+                                                    <HighlightOffIcon />
+                                                </IconButton>
+                                            </InputAdornment>
+                                        )
+                                    }
+                                />
+                            </FormControl>
+                            <Stack direction="row" spacing={1}>
+                                <Chip
+                                    label="Texture2D"
+                                    color="primary"
+                                    variant={filterChips[28] ? "filled" : "outlined"}
+                                    onClick={() => handleChipFiltering(28)}
+                                />
+                                <Chip
+                                    label="Mesh"
+                                    color="primary"
+                                    variant={filterChips[43] ? "filled" : "outlined"}
+                                    onClick={() => handleChipFiltering(43)}
+                                />
+                            </Stack>
                             <AssetList
-                                assetList={assetList}
+                                assetList={visibleAssetList}
                                 handleDisplayedAsset={handleDisplayedAsset}
                             />
                         </Box>
@@ -108,4 +215,4 @@ function AssetViewer(props: Props) {
 }
 
 export default AssetViewer;
-export type { AssetListJSON };
+export type { AssetListItem };
--- src/pages/asset-viewer/components/AssetScrollList.tsx
+++ src/pages/asset-viewer/components/AssetScrollList.tsx
@@ -1,26 +1,75 @@
-import { List, ListSubheader } from "@mui/material";
-import { AssetListJSON } from "../AssetViewer";
-import ScrollListItem from "./ScrollListItem";
+import { ListItem, ListItemButton, ListItemIcon, ListItemText, Tooltip } from "@mui/material";
+import { AssetListItem } from "../AssetViewer";
+import { FixedSizeList } from "react-window";
+import FolderIcon from "@mui/icons-material/Folder";
+import PhotoIcon from "@mui/icons-material/Photo";
+import ViewInArIcon from "@mui/icons-material/ViewInAr";
+import { ClassID } from "@extractor/consts";
 
 interface Props {
-    assetList: AssetListJSON;
+    assetList: Array<AssetListItem>;
     handleDisplayedAsset: (containerPath: string, pathId: string, classId: number) => void;
 }
+
+interface ScrollListItemProps {
+    index: number;
+    style: React.CSSProperties;
+    item: AssetListItem;
+}
+
 function AssetList(props: Props) {
+    const { assetList, handleDisplayedAsset } = props;
+
+    const ScrollListItem = (props: ScrollListItemProps) => {
+        const { index, style, item } = props;
+
+        if (item.type === "container") {
+            return (
+                <ListItem style={{ ...style, paddingLeft: 2 }} key={index} dense>
+                    <Tooltip title={item.container} placement="top" enterDelay={500}>
+                        <ListItemIcon sx={{ minWidth: 30 }}>
+                            <FolderIcon />
+                        </ListItemIcon>
+                    </Tooltip>
+                    <ListItemText>{item.name}</ListItemText>
+                </ListItem>
+            );
+        } else {
+            return (
+                <ListItem style={style} key={index} dense>
+                    <ListItemButton
+                        sx={{ paddingLeft: 3 }}
+                        onClick={() =>
+                            handleDisplayedAsset(
+                                item.container,
+                                item.asset.pathId,
+                                item.asset.classId
+                            )
+                        }
+                    >
+                        <ListItemIcon sx={{ minWidth: 30 }}>
+                            {item.asset?.classId === ClassID.Texture2D && <PhotoIcon />}
+                            {item.asset?.classId === ClassID.Mesh && <ViewInArIcon />}
+                        </ListItemIcon>
+                        <ListItemText>{item.name}</ListItemText>
+                    </ListItemButton>
+                </ListItem>
+            );
+        }
+    };
+
     return (
-        <List
-            sx={{ overflowY: "auto", maxHeight: "750px" }}
-            subheader={<ListSubheader>Containers and assets</ListSubheader>}
-            dense
+        <FixedSizeList
+            height={750}
+            width={400}
+            itemCount={assetList.length}
+            itemSize={35}
+            style={{ marginLeft: 0, overflowY: "scroll" }}
         >
-            {props.assetList.map((container, index) => (
-                <ScrollListItem
-                    key={index}
-                    container={container}
-                    handleDisplayedAsset={props.handleDisplayedAsset}
-                />
-            ))}
-        </List>
+            {({ index, style }) => (
+                <ScrollListItem index={index} style={style} item={assetList[index]} />
+            )}
+        </FixedSizeList>
     );
 }
 
